ca25269ee41eee62767c586f92cd383ca7759bab
Alan21103 <syahlansaputra03@gmail.com>
2025-01-12 02:54:02 +0700

    Membuat form mahasiswa untuk menginput data mahasiswa

--- app/src/main/java/com/example/mobilefirebase/ui/view/InsertView.kt
+++ app/src/main/java/com/example/mobilefirebase/ui/view/InsertView.kt
@@ -3,13 +3,18 @@ package com.example.mobilefirebase.ui.view
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
@@ -22,13 +27,16 @@ import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.mobilefirebase.ui.viewmodel.FormErrorState
 import com.example.mobilefirebase.ui.viewmodel.FormState
 import com.example.mobilefirebase.ui.viewmodel.InsertUiState
 import com.example.mobilefirebase.ui.viewmodel.InsertViewModel
 import com.example.mobilefirebase.ui.viewmodel.MahasiswaEvent
 import com.example.mobilefirebase.ui.viewmodel.PenyediaViewModel
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -143,6 +151,100 @@ fun InsertBodyMhs(
                 Text("Add")
             }
         }
+    }
+}
+
+@Composable
+fun FormMahasiswa(
+    mahasiswaEvent: MahasiswaEvent = MahasiswaEvent(),
+    onValueChange: (MahasiswaEvent) -> Unit,
+    errorState: FormErrorState = FormErrorState(),
+    modifier: Modifier = Modifier
+){
+    val gender = listOf("Laki-laki", "Perempuan")
+    val kelas = listOf("A", "B", "C", "D", "E")
+
+    Column(
+        modifier = modifier.fillMaxWidth()
+    ) {
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.nama,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(nama = it)) },
+            label = { Text("Nama") },
+            isError = errorState.nama != null,
+            placeholder = { Text("Masukkan nama") }
+        )
+        Text(text = errorState.nama ?: "", color = Color.Red)
+
+        // NIM Field
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.nim,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(nim = it)) },
+            label = { Text("NIM") },
+            isError = errorState.nim != null,
+            placeholder = { Text("Masukkan NIM") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.nim ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Jenis Kelamin
+        Text(text = "Jenis Kelamin")
+        Row(modifier = Modifier.fillMaxWidth()) {
+            gender.forEach { gd ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = mahasiswaEvent.gender == gd,
+                        onClick = { onValueChange(mahasiswaEvent.copy(gender = gd)) }
+                    )
+                    Text(text = gd)
+                }
+            }
+        }
+        Text(text = errorState.gender ?: "", color = Color.Red)
+
+        // Alamat Field
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.alamat,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(alamat = it)) },
+            label = { Text("Alamat") },
+            isError = errorState.alamat != null,
+            placeholder = { Text("Masukkan alamat") }
+        )
+        Text(text = errorState.alamat ?: "", color = Color.Red)
+
+        Spacer(modifier = Modifier.height(16.dp))
+
+        // Kelas
+        Text(text = "Kelas")
+        Row {
+            kelas.forEach { kls ->
+                Row(verticalAlignment = Alignment.CenterVertically) {
+                    RadioButton(
+                        selected = mahasiswaEvent.kelas == kls,
+                        onClick = { onValueChange(mahasiswaEvent.copy(kelas = kls)) }
+                    )
+                    Text(text = kls)
+                }
+            }
+        }
+        Text(text = errorState.kelas ?: "", color = Color.Red)
+
+        // Angkatan Field
+        OutlinedTextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = mahasiswaEvent.angkatan,
+            onValueChange = { onValueChange(mahasiswaEvent.copy(angkatan = it)) },
+            label = { Text("Angkatan") },
+            isError = errorState.angkatan != null,
+            placeholder = { Text("Masukkan angkatan") },
+            keyboardOptions = KeyboardOptions(keyboardType = KeyboardType.Number)
+        )
+        Text(text = errorState.angkatan ?: "", color = Color.Red)
     }
 }
 
