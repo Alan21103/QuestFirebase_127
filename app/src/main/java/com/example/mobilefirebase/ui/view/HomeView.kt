7310721060ee9cbbd549b16836bd9b7f3c515b92
Alan21103 <syahlansaputra03@gmail.com>
2025-01-12 04:14:04 +0700

    Menambahkan top app bar pada home view

--- app/src/main/java/com/example/mobilefirebase/ui/view/HomeView.kt
+++ app/src/main/java/com/example/mobilefirebase/ui/view/HomeView.kt
@@ -28,6 +28,7 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -58,7 +59,11 @@ fun HomeScreen(
 
     Scaffold(
         modifier = modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
-        topBar = {},
+        topBar = {
+            TopAppBar(
+                title = { Text("Tambah Mahasiswa") },
+            )
+        },
         floatingActionButton = {
             FloatingActionButton(
                 onClick = navigateToItemEntry,
